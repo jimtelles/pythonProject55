fcf16d74cc908cc6bdbab906209a203312a94a27
Jim Telles <tellesj@oregonstate.edu>
2023-06-28 11:07:45 -0700

    again

---
+++ hello_gradescope.py
@@ -0,0 +1,15 @@
+# Name: Jim Telles
+# OSU Email: tellesj@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: Gradescope check
+# Due Date: 7.3.23
+# Description: Checking to see if I have a sufficient version of Python installed as well as a connection to the
+#               gradescope assignment checker. And seeing if I can pull it from github.
+import sys
+
+
+def gradescope_test():
+    return 42
+
+
+output = sys.version_info
